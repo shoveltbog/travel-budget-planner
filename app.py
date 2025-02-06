ff2f9e00ebb621e5044a445bd8a98e0fdd69e70e
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2025-02-06 19:30:05 +1100

    added basic user input form

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/budget', methods=['POST'])
+def budget():
+    destination = request.form['destination']
+    budget = float(request.form['budget'])
+    duration = int(request.form['duration'])
+
+    return f"You are planning a {duration}-day trip to {destination} with a budget of ${budget}."
 
 if __name__ == '__main__':
     app.run(debug=True)
